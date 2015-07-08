9a928cbed89def97e1c0a211c74ce6badc86fded
sidhoney <sidharthagopinath@Sid.local>
2015-07-08 09:23:32 -0400

    yDoll Android Application

---
+++ app/src/main/java/com/sidhartha/ydoll/WebSiteFragment.java
@@ -0,0 +1,42 @@
+package com.sidhartha.ydoll;
+
+import com.sidhartha.ydoll.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebSiteFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+
+		View rootView = inflater.inflate(R.layout.fragment_website, container, false);
+		WebView mainWebView = (WebView) rootView.findViewById(R.id.mainWebView);
+
+        WebSettings webSettings = mainWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        
+        mainWebView.setWebViewClient(new MyCustomWebViewClient());
+        mainWebView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        mainWebView.loadUrl("http://www.ydoll.org/");
+		
+		return rootView;
+	}
+
+	
+	
+	private class MyCustomWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+	}
+}
